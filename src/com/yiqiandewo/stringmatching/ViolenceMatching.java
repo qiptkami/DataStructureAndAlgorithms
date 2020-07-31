6bb206676278fb74a2f449dea293711abbe474a1
yiqiandewo <1429711247@qq.com>
2020-07-31 23:44:22 +0800

    暴力字符串匹配

--- src/com/yiqiandewo/stringmatching/ViolenceMatching.java
+++ src/com/yiqiandewo/stringmatching/ViolenceMatching.java
@@ -1,4 +1,24 @@
 package com.yiqiandewo.stringmatching;
 
 public class ViolenceMatching {
+    public static void main(String[] args) {
+        String s = "BBC ABCDAB ABCDABCDABDE";
+        String p = "ABCDABD";
+
+        System.out.println(violenceMatching(s, p));
+    }
+
+    public static boolean violenceMatching(String s, String p) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < p.length()) {
+            if (s.charAt(i+j) == p.charAt(j)) {
+                j++;
+            } else {
+                i++;
+                j = 0;
+            }
+        }
+
+        return j == p.length();
+    }
 }
