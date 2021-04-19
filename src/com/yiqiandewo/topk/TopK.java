eda8385828355baa2f2a89cc7b5e31cf3ca20d24
yiqiandewo <1429711247@qq.com>
2021-04-19 16:06:00 +0800

    topk

--- src/com/yiqiandewo/topk/TopK.java
+++ src/com/yiqiandewo/topk/TopK.java
@@ -15,6 +15,52 @@ public class TopK {
         return quickSelect(arr, k - 1, 0, arr.length - 1);
     }
 
+
+    public int topK(int[] arr, int k) {
+        buildHeap(arr);
+
+        for (int i = arr.length - 1; i > arr.length - k; i--) {
+            //第一个节点和最后一个节点交换
+            swap(arr, 0, i);
+            //当前堆被破坏，重新构造堆
+            heapify(arr, 0, i);
+        }
+
+        return arr[0];
+    }
+
+
+    private void buildHeap(int[] arr) {
+        //首先找到最后一下父节点
+        int parent = (arr.length - 1) / 2;
+
+        //自底向上构建堆
+        for (int i = parent; i >= 0; i--) {
+            heapify(arr, i, arr.length);
+        }
+    }
+
+    private void heapify(int[] arr, int i, int len) { //i是parent节点的下标, len是要对多少个节点构建堆
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int max = i;
+
+        if (left < len && arr[max] < arr[left]) {
+            max = left;
+        }
+
+        if (right < len && arr[max] < arr[right]) {
+            max = right;
+        }
+
+        if (max != i) {
+            swap(arr, max, i);
+
+            //对被破坏的子树重新构建大顶堆
+            heapify(arr, max, len);
+        }
+    }
+
     private int quickSelect(int[] arr, int k, int left, int right) {
         int res = partition(arr, left, right);
         if (res == k) {
